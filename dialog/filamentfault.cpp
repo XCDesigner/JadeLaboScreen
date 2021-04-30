8ad7c458092a0af49f20a8c9bfd5279fe8c45831
xcdesigner <sf-bvc@163.com>
2021-05-01 00:11:32 +0800

    NEW:Add funtions for buttons

--- dialog/filamentfault.cpp
+++ dialog/filamentfault.cpp
@@ -25,15 +25,21 @@ void filamentFault::show()
 
 void filamentFault::on_btnResume_clicked()
 {
-
+    ret_value.append(QByteArray("Resume"));
+    hide();
+    emit hideWidget();
 }
 
 void filamentFault::on_btnChangeFilament_clicked()
 {
-
+    ret_value.append(QByteArray("ChangeFilament"));
+    hide();
+    emit hideWidget();
 }
 
 void filamentFault::on_btnAbort_clicked()
 {
-
+    ret_value.append(QByteArray("Abort"));
+    hide();
+    emit hideWidget();
 }
