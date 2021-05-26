43f482c6ccb16b02e0214a7c53164faac43d8ee7
xcdesigner <sf-bvc@163.com>
2021-05-26 17:27:50 +0800

    Bugfix:Nozzle turning label

--- pages/About/About.cpp
+++ pages/About/About.cpp
@@ -35,7 +35,7 @@ void MainWindow::aboutTimerTester()
 {
     if(ui->checkBox->isChecked() == true) {
         QByteArray regs = QByteArray::fromHex("001241");
-        m_port->getTMCValue(3, regs);
+        m_port->getTMCValue(0, regs);
         m_port->trigFILSample();
         m_port->getFILValue();
     }
